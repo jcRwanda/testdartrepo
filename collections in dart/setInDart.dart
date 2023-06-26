f265f25008b670ae03b25ba9fc2862e546933832
jcRwanda <jeancloudenizeyimana1@gmail.com>
2023-06-26 20:03:29 +0200

    collection in dart

---
+++ collections in dart/setInDart.dart
@@ -0,0 +1,51 @@
+// void main() {
+//   // declaring fruits as Set
+//   Set<String> fruits = {"Apple", "Orange", "Mango", "Banana"};
+
+//   // using different properties of Set
+//   print("First Value is ${fruits.first}");
+//   print("Last Value is ${fruits.last}");
+//   print("Is fruits empty? ${fruits.isEmpty}");
+//   print("Is fruits not empty? ${fruits.isNotEmpty}");
+//   print("The length of fruits is ${fruits.length}");
+// }
+
+// //If you want to see whether the Set contains specific items or not, you can use the contains method
+
+// void main() {
+//   Set<String> fruits = {"Apple", "Orange", "Mango"};
+//   print(fruits.contains("Mango"));
+//   print(fruits.contains("Lemon"));
+// }
+
+// void main() {
+//   Set<String> fruits = {"Apple", "Orange", "Mango"};
+//   // to clear all items
+//   fruits.clear();
+
+//   print(fruits);
+// }
+
+// void main() {
+//   Set<String> fruits1 = {"Apple", "Orange", "Mango"};
+//   Set<String> fruits2 = {"Apple", "Grapes", "Banana"};
+
+//   final differenceSet = fruits1.difference(fruits2);
+
+//   print(differenceSet);
+// }
+
+// void main() {
+//   Set<String> days = {"Sunday", "Monday", "Tuesday"};
+//   // index starts from 0 so 2 means Tuesday
+//   print(days.elementAt(2));
+// }
+
+// void main() {
+//   Set<String> fruits1 = {"Apple", "Orange", "Mango"};
+//   Set<String> fruits2 = {"Apple", "Grapes", "Banana"};
+
+//   final intersectionSet = fruits1.intersection(fruits2);
+
+//   print(intersectionSet);
+// }
